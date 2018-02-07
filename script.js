53a35a4650d35a6ac75193edf373470f8d1655b3
REALmyenemy <eldani765@hotmail.com>
2018-02-07 23:50:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function cambiar_fondo()
+{
+	document.getElementById("uno").style.backgroundColor="red";
+}
+function alertar()
+{
+	alert("Has dejado el segundo div");
+}
+function mostrar_fecha()
+{
+	document.write(Date());
+}
